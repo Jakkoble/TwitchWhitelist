d9baf5cea97d7a3e7687d9cbed9c2c58ee944cc7
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-08-11 22:45:18 +0200

    Added Used Whitelist Check ✅

--- src/main/java/de/jakkoble/Whitelist.kt
+++ src/main/java/de/jakkoble/Whitelist.kt
@@ -10,4 +10,10 @@ object Whitelist {
       return true
    }
    fun OfflinePlayer.isListed(): Boolean = TwitchWhitelist.instance.config.getStringList("whitelistedPlayer").contains(this.uniqueId.toString())
+   fun OfflinePlayer.usedWhitelist(): Boolean {
+      TwitchWhitelist.instance.config.getConfigurationSection("alreadyWhitelisted")?.getValues(false)?.values?.forEach {
+         if (it.equals(this.uniqueId.toString())) return true
+      }
+      return false
+   }
 }
