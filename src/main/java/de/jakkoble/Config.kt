4d27b7bd3d41fecabc4d1d54c7cb0e3c85748ff6
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-08-11 22:51:53 +0200

    Added Config Values ⚙️

--- src/main/java/de/jakkoble/Config.kt
+++ src/main/java/de/jakkoble/Config.kt
@@ -7,16 +7,18 @@ class Config {
       if (!File("plugins/TwitchWhitelist/config.yml").exists()) {
          val config = TwitchWhitelist.instance.config
          config.set("token", "YourToken")
-         config.set("channel", "Jakkoble")
+         config.set("channel", "YourChannelID")
          config.set("serverName", "Minecraft Community SMP")
          config.set("chanelRewardName", "Minecraft Server Access")
          config.set("sendResponseMessage", true)
-         config.set("responseMessage", "%s you are now Whitelisted on the %s. // %s for twitchUserName and second %s for serverName")
-         config.set("notWhitelistedMessage", "&aYou are not Whitelisted on this Server!")
+         config.set("successResponseMessage", "/me %s, you are now Whitelisted on the '%s'.")
+         config.set("alreadyWhitelistedResponseMessage", "/me %s, you are already Whitelisted on the '%s'.")
+         config.set("alreadyWhitelistedOnePlayerResponseMessage", "/me %s, you have already Whitelisted one Player to the '%s'.")
+         config.set("notWhitelistedMessage", "§cYou are not Whitelisted on this Server! Purchase a Whitelist Slot via Channel Points Reward at: %s")
          TwitchWhitelist.instance.saveConfig()
       }
    }
-   fun getData(path: String): String = TwitchWhitelist.instance.config.getString(path) ?: "Not found"
+   fun getData(path: String): String = TwitchWhitelist.instance.config.getString(path) ?: "Message not Found"
    fun setData(path: String, value: Any) {
       TwitchWhitelist.instance.config.set(path, value)
       TwitchWhitelist.instance.saveConfig()
