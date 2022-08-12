5521469d457b45f02bbfc55e8fd498878a12f709
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-08-12 18:53:51 +0200

    Owner Check ✅

--- src/main/java/de/jakkoble/Whitelist.kt
+++ src/main/java/de/jakkoble/Whitelist.kt
@@ -3,15 +3,17 @@ package de.jakkoble
 import org.bukkit.OfflinePlayer
 
 object Whitelist {
-   fun OfflinePlayer.whitelist(): Boolean {
+   fun OfflinePlayer.whitelist(userID: String): Boolean {
       if (this.isListed()) return false
-      val whitelistedPlayer = TwitchWhitelist.instance.config.getStringList("whitelistedPlayer")
-      whitelistedPlayer.add(this.uniqueId.toString())
+      val id = if (userID == Config().getData("channelID"))
+         TwitchWhitelist.instance.config.getConfigurationSection("whitelistedPlayer")?.getKeys(false)?.maxOfOrNull { it.toInt() }?.plus(1) ?: 0
+      else userID
+      Config().setData("whitelistedPlayer.$id", this.uniqueId.toString())
       return true
    }
-   fun OfflinePlayer.isListed(): Boolean = TwitchWhitelist.instance.config.getStringList("whitelistedPlayer").contains(this.uniqueId.toString())
+   fun OfflinePlayer.isListed(): Boolean = TwitchWhitelist.instance.config.getConfigurationSection("whitelistedPlayer")?.getValues(false)?.values?.contains(this.uniqueId.toString()) ?: false
    fun String.usedWhitelist(): Boolean {
-      TwitchWhitelist.instance.config.getConfigurationSection("alreadyWhitelisted")?.getKeys(false)?.forEach {
+      TwitchWhitelist.instance.config.getConfigurationSection("whitelistedPlayer")?.getKeys(false)?.forEach {
          if (it.equals(this)) return true
       }
       return false
