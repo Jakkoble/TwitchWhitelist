d6dac80f5e2c4b3070b00a3f978f5a9d2eede4ee
Jakkoble <business@jakkoble.de>
2022-09-06 22:04:11 +0200

    Updated Default Config ⬆️

--- src/main/java/de/jakkoble/Config.kt
+++ src/main/java/de/jakkoble/Config.kt
@@ -6,19 +6,22 @@ class Config {
    init {
       if (!File("plugins/TwitchWhitelist/config.yml").exists()) {
          val config = TwitchWhitelist.instance.config
+         config.set("enabled", true)
          config.set("token", "YourToken")
          config.set("channelID", "YourChannelID")
+         config.set("ticketsPerUser", 1)
          config.set("serverName", "Minecraft Community SMP")
-         config.set("chanelRewardName", "Minecraft Server Access")
+         config.set("chanelRewardName", "Minecraft Whitelist Ticket")
          config.set("sendResponseMessage", true)
          config.set("successResponseMessage", "/me %s, you are now Whitelisted on the '%s'.")
+         config.set("noPlayerFoundResponseMessage", "/me %s, there is no Player called '%s'.")
          config.set("alreadyWhitelistedResponseMessage", "/me %s, you are already Whitelisted on the '%s'.")
-         config.set("alreadyWhitelistedOnePlayerResponseMessage", "/me %s, you have already Whitelisted one Player to the '%s'.")
-         config.set("notWhitelistedMessage", "§cYou are not Whitelisted on this Server! Purchase a Whitelist Slot via Channel Points Reward at %s")
+         config.set("alreadyWhitelistedOnePlayerResponseMessage", "/me %s, you have already Whitelisted %s to the '%s'.")
+         config.set("notWhitelistedMessage", "§cYou are not Whitelisted on this Server! Purchase a Whitelist Slot via Channel Points Reward at '%s'.")
          TwitchWhitelist.instance.saveConfig()
       }
    }
-   fun getData(path: String): String = TwitchWhitelist.instance.config.getString(path) ?: "Message not Found"
+   fun getData(path: String): String = TwitchWhitelist.instance.config.getString(path) ?: "Content not Found"
    fun setData(path: String, value: Any) {
       TwitchWhitelist.instance.config.set(path, value)
       TwitchWhitelist.instance.saveConfig()
