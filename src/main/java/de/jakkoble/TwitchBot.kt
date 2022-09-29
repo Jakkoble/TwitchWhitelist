76c860b579ad4d8273478065134b26e0ec292a43
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-09-29 13:11:41 +0200

    Increased Performance 🏎️

--- src/main/java/de/jakkoble/TwitchBot.kt
+++ src/main/java/de/jakkoble/TwitchBot.kt
@@ -10,16 +10,16 @@ class TwitchBot {
    private lateinit var twitchClient: TwitchClient
    private var available = true
    private lateinit var userName: String
-   private val token = Config().getData("token")
+   private val token = chatToken
    private val credential = OAuth2Credential("twitch", token)
    fun connect() {
-      if (token.length != 30 || Config().getData("channelID") == "YourChannelID") {
-         TwitchWhitelist.instance.server.consoleSender.sendMessage("")
-         TwitchWhitelist.instance.server.consoleSender.sendMessage("${ChatColor.DARK_RED}You have not set the 'Bot Chat Token' or 'ChannelID' in the Config yet! Without these it will not work.")
-         TwitchWhitelist.instance.server.consoleSender.sendMessage("${ChatColor.DARK_RED}To get the Access Token, visit: https://twitchtokengenerator.com/")
-         TwitchWhitelist.instance.server.consoleSender.sendMessage("${ChatColor.DARK_RED}To get the ChannelID aka TwitchID, visit: https://www.streamweasels.com/tools/convert-twitch-username-to-user-id/")
-         TwitchWhitelist.instance.server.consoleSender.sendMessage("${ChatColor.DARK_RED}Stop the Server, Setup the Config for your needs and Start the Server again.")
-         TwitchWhitelist.instance.server.consoleSender.sendMessage("")
+      if (token.length != 30 || channelID == "YourChannelID") {
+         TwitchWhitelist.INSTANCE.server.consoleSender.sendMessage("")
+         TwitchWhitelist.INSTANCE.server.consoleSender.sendMessage("${ChatColor.DARK_RED}You have not set the 'Bot Chat Token' or 'ChannelID' in the Config yet! Without these it will not work.")
+         TwitchWhitelist.INSTANCE.server.consoleSender.sendMessage("${ChatColor.DARK_RED}To get the Access Token, visit: https://twitchtokengenerator.com/")
+         TwitchWhitelist.INSTANCE.server.consoleSender.sendMessage("${ChatColor.DARK_RED}To get the ChannelID aka TwitchID, visit: https://www.streamweasels.com/tools/convert-twitch-username-to-user-id/")
+         TwitchWhitelist.INSTANCE.server.consoleSender.sendMessage("${ChatColor.DARK_RED}Stop the Server, Setup the Config for your needs and Start the Server again.")
+         TwitchWhitelist.INSTANCE.server.consoleSender.sendMessage("")
          available = false
          return
       }
@@ -29,7 +29,7 @@ class TwitchBot {
          .withEnableHelix(true)
          .withChatAccount(credential)
          .build()
-      userName = getChannelofID(Config().getData("channelID"))
+      userName = getChannelofID(channelID)
       if (!twitchClient.chat.isChannelJoined(userName)) twitchClient.chat.joinChannel(userName)
       registerEvent()
    }
@@ -39,44 +39,41 @@ class TwitchBot {
       twitchClient.close()
    }
    private fun registerEvent() {
-      twitchClient.pubSub.listenForChannelPointsRedemptionEvents(credential, Config().getData("channelID"))
+      twitchClient.pubSub.listenForChannelPointsRedemptionEvents(credential, channelID)
       twitchClient.eventManager.onEvent(RewardRedeemedEvent::class.java) { event: RewardRedeemedEvent ->
-         if (event.redemption.reward.title.equals(Config().getData("chanelRewardName"))) {
+         if (event.redemption.reward.title.equals(channelRewardName)) {
             val playerName = event.redemption.userInput ?: return@onEvent
             val userID = event.redemption.user.id ?: return@onEvent
-            val respond = Config().getData("sendResponseMessage").toBoolean()
             val userData = playerName.getUserDataFromName()
             if (userData == null || playerName.length > 25) {
-               if (respond) twitchClient.chat.sendMessage(getChannelofID(Config().getData("channelID")), String.format(
-                  Config().getData("noPlayerFoundResponseMessage"),
+               if (sendMessage) twitchClient.chat.sendMessage(getChannelofID(channelID), String.format(
+                  playerNotFoundMessage,
                   event.redemption.user.displayName,
                   playerName))
-               TwitchWhitelist.instance.server.consoleSender.sendMessage("${ChatColor.YELLOW}There is no Player called $playerName.")
+               TwitchWhitelist.INSTANCE.server.consoleSender.sendMessage("${ChatColor.YELLOW}There is no Player called $playerName.")
                return@onEvent
             }
             val uuid = userData.id
             if (Whitelist().usedWhitelist(userID)) {
-               if (respond) twitchClient.chat.sendMessage(getChannelofID(Config().getData("channelID")), String.format(
-                     Config().getData("alreadyWhitelistedOnePlayerResponseMessage"),
+               if (sendMessage) twitchClient.chat.sendMessage(getChannelofID(channelID), String.format(
+                     alreadyWhitelistedOnePlayerMessage,
                      event.redemption.user.displayName,
-                     if(Config().getData("ticketsPerUser").toInt() == 1) "one Player" else "${Config().getData("ticketsPerUser")} Players",
-                     Config().getData("serverName")))
-               TwitchWhitelist.instance.server.consoleSender.sendMessage("${ChatColor.YELLOW}User ${event.redemption.user.displayName} already Whitelisted one Player.")
+                     if(ticketPerUser == 1) "one Player" else "$ticketPerUser Players",
+                     serverName))
+               TwitchWhitelist.INSTANCE.server.consoleSender.sendMessage("${ChatColor.YELLOW}User ${event.redemption.user.displayName} already Whitelisted one Player.")
                return@onEvent
             }
-
             if (!Whitelist().whitelist(UserData(playerName, uuid, userID))) {
-               if (respond) twitchClient.chat.sendMessage(
-                  getChannelofID(Config().getData("channelID")),
-                  String.format(Config().getData("alreadyWhitelistedResponseMessage"), event.redemption.user.displayName, Config().getData("serverName")))
-               TwitchWhitelist.instance.server.consoleSender.sendMessage("${ChatColor.YELLOW}Player $playerName is already Whitelisted.")
+               if (sendMessage) twitchClient.chat.sendMessage(
+                  getChannelofID(channelID),
+                  String.format(alreadyWhitelistedMessage, event.redemption.user.displayName, serverName))
+               TwitchWhitelist.INSTANCE.server.consoleSender.sendMessage("${ChatColor.YELLOW}Player $playerName is already Whitelisted.")
                return@onEvent
             }
-
-            TwitchWhitelist.instance.server.consoleSender.sendMessage("${ChatColor.GREEN}Added Player $playerName to the Whitelist.")
-            if (respond) twitchClient.chat.sendMessage(
-               getChannelofID(Config().getData("channelID")),
-               String.format(Config().getData("successResponseMessage"), event.redemption.user.displayName, Config().getData("serverName")))
+            TwitchWhitelist.INSTANCE.server.consoleSender.sendMessage("${ChatColor.GREEN}Added Player $playerName to the Whitelist.")
+            if (sendMessage) twitchClient.chat.sendMessage(
+               getChannelofID(channelID),
+               String.format(successMessage, event.redemption.user.displayName, serverName))
          }
       }
    }
--- src/main/java/de/jakkoble/WhitelistCommand.kt
+++ src/main/java/de/jakkoble/WhitelistCommand.kt
@@ -10,96 +10,93 @@ class WhitelistCommand : CommandExecutor, TabCompleter {
    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
       if (args.isEmpty()) {
          sender.sendUsage()
-         return false
+         return true
       }
       when(args[0]) {
          "on" -> {
-            if (Config().getData("enabled").toBoolean()) {
-               sender.sendMessage("${prefix}TwitchWhitelist is already turned on.")
-               return false
+            if (enabled) {
+               sender.sendMessage("$prefix TwitchWhitelist is already turned on.")
+               return true
             }
-            Config().setData("enabled", true)
-            sender.sendMessage("${prefix}You have enabled the TwitchWhitelist.")
-            return false
+            Config().setData(ConfigEntry.ENABLED, true)
+            sender.sendMessage("$prefix You have enabled the TwitchWhitelist.")
+            return true
          }
          "off" -> {
-            if (!Config().getData("enabled").toBoolean()) {
-               sender.sendMessage("${prefix}TwitchWhitelist is already turned off.")
-               return false
+            if (!enabled) {
+               sender.sendMessage("$prefix TwitchWhitelist is already turned off.")
+               return true
             }
-            Config().setData("enabled", false)
-            sender.sendMessage("${prefix}You have disabled the TwitchWhitelist.")
-            return false
+            Config().setData(ConfigEntry.ENABLED, false)
+            sender.sendMessage("$prefix You have disabled the TwitchWhitelist.")
+            return true
          }
          "list" -> {
             sender.sendMessage("")
-            if (Whitelist().playerNames().isEmpty()) sender.sendMessage("No Player is Whitelisted")
+            val playerNames = Whitelist().playerNames()
+            if (playerNames.isEmpty()) sender.sendMessage("No Player is Whitelisted")
             else {
-               val playerNames = Whitelist().playerNames()
                sender.sendMessage("${ChatColor.GOLD}There ${if(playerNames.size == 1) "is one Player" else "are ${playerNames.size} Players"} whitelisted.")
                val message = StringBuilder()
                playerNames.forEach { message.append(if (playerNames.indexOf(it) != playerNames.size - 1) "$it, " else it) }
                sender.sendMessage(message.substring(0))
             }
             sender.sendMessage("")
-            return false
+            return true
          }
       }
       if (args.size != 2) {
          sender.sendUsage()
-         return false
+         return true
       }
+      val playerName = args[1]
       when(args[0]) {
          "add" -> {
-            val userData = args[1].getUserDataFromName()
-            if (userData == null || args[1].length > 25) {
-               sender.sendMessage("${prefix}There is no Player called ${args[1]}")
-               TwitchWhitelist.instance.server.consoleSender.sendMessage("${ChatColor.YELLOW}There is no Player called ${args[1]}.")
-               return false
+            val userData = Whitelist().userDataByName(playerName)
+            if (userData == null || playerName.length > 25) {
+               sender.sendMessage("$prefix There is no Player called $playerName")
+               TwitchWhitelist.INSTANCE.server.consoleSender.sendMessage("${ChatColor.YELLOW}There is no Player called $playerName.")
+               return true
             }
-            val uuid = userData.id
             if (Whitelist().whitelist(UserData(
-                  name = args[1],
-                  uuid = uuid,
-                  twitchUserID = Config().getData("channelID")))) {
-               sender.sendMessage("${prefix}You have added ${args[1]} to the Whitelist.")
-               TwitchWhitelist.instance.server.consoleSender.sendMessage("${ChatColor.GREEN}Added Player ${args[1]} to the Whitelist.")
+                  name = playerName,
+                  uuid = userData.uuid,
+                  twitchUserID = channelID))) {
+               sender.sendMessage("$prefix You have added $playerName to the Whitelist.")
+               TwitchWhitelist.INSTANCE.server.consoleSender.sendMessage("${ChatColor.GREEN}Added Player $playerName to the Whitelist.")
             } else {
-               sender.sendMessage("${prefix}The Player ${args[1]} is already Whitelisted.")
-               TwitchWhitelist.instance.server.consoleSender.sendMessage("${ChatColor.YELLOW}Player ${args[1]} is already Whitelisted.")
+               sender.sendMessage("$prefix The Player $playerName is already Whitelisted.")
+               TwitchWhitelist.INSTANCE.server.consoleSender.sendMessage("${ChatColor.YELLOW}Player $playerName is already Whitelisted.")
             }
          }
          "remove" -> {
-            val userData = args[1].getUserDataFromName()
-            if (userData == null || args[1].length > 25) {
-               TwitchWhitelist.instance.server.consoleSender.sendMessage("${ChatColor.YELLOW}There is no Player called ${args[1]}.")
-               sender.sendMessage("${prefix}There is no Player called ${args[1]}")
-               return false
+            val userData = Whitelist().userDataByName(playerName)
+            if (userData == null || playerName.length > 25) {
+               TwitchWhitelist.INSTANCE.server.consoleSender.sendMessage("${ChatColor.YELLOW}There is no Player called $playerName.")
+               sender.sendMessage("$prefix There is no Player called $playerName")
+               return true
             }
-            val uuid = userData.id
-            if (Whitelist().unwhitelist(uuid)) {
-               sender.sendMessage("${prefix}You have removed ${args[1]} from the Whitelist.")
-               TwitchWhitelist.instance.server.consoleSender.sendMessage("${ChatColor.RED}Player ${args[1]} removed from Whitelist.")
+            if (Whitelist().unwhitelist(userData.uuid)) {
+               sender.sendMessage("$prefix You have removed $playerName from the Whitelist.")
+               TwitchWhitelist.INSTANCE.server.consoleSender.sendMessage("${ChatColor.RED}Player $playerName removed from Whitelist.")
             } else {
-               sender.sendMessage("${prefix}The Player ${args[1]} is not Whitelisted.")
-               TwitchWhitelist.instance.server.consoleSender.sendMessage("${ChatColor.YELLOW}Player ${args[1]} is not Whitelisted.")
+               sender.sendMessage("$prefix The Player $playerName is not Whitelisted.")
+               TwitchWhitelist.INSTANCE.server.consoleSender.sendMessage("${ChatColor.YELLOW}Player $playerName is not Whitelisted.")
             }
          }
       }
-      return false
+      return true
    }
-
    private fun CommandSender.sendUsage() {
       sendMessage("")
-      sendMessage("${ChatColor.GOLD}Usage")
-      sendMessage("${ChatColor.WHITE}whitelist on")
-      sendMessage("${ChatColor.WHITE}whitelist off")
-      sendMessage("${ChatColor.WHITE}whitelist list")
-      sendMessage("${ChatColor.WHITE}whitelist add playerName")
-      sendMessage("${ChatColor.WHITE}whitelist remove playerName")
+      sendMessage("$prefix ${ChatColor.YELLOW}Usage")
+      sendMessage("$prefix whitelist on")
+      sendMessage("$prefix whitelist off")
+      sendMessage("$prefix whitelist list")
+      sendMessage("$prefix whitelist add playerName")
+      sendMessage("$prefix whitelist remove playerName")
       sendMessage("")
    }
-
    override fun onTabComplete(sender: CommandSender, command: Command, alias: String, args: Array<out String>): MutableList<String> {
       val tabList = mutableListOf<String>()
       if (args.size == 1) {
