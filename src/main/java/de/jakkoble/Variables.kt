6e74e07de284f2e411b46e1a3b12116127d7c2a7
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-09-29 13:10:48 +0200

    Added Variables.kt ℹ️

---
+++ src/main/java/de/jakkoble/Variables.kt
@@ -0,0 +1,17 @@
+package de.jakkoble
+
+import org.bukkit.ChatColor
+
+val prefix = "${ChatColor.GOLD}Whitelist ${ChatColor.WHITE}•${ChatColor.GRAY}"
+var enabled: Boolean = true
+var chatToken: String = ""
+var channelID: String = ""
+var ticketPerUser: Int = 1
+var serverName: String = ""
+var channelRewardName: String = ""
+var sendMessage: Boolean = true
+var successMessage: String = ""
+var playerNotFoundMessage: String = ""
+var alreadyWhitelistedMessage: String = ""
+var alreadyWhitelistedOnePlayerMessage: String = ""
+var notWhitelistedText: String = ""
