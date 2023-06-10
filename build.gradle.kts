242b6ebc27e730f0b0cf8e2f1010a4fbb39c14e0
Jakkoble <business@jakkoble.de>
2023-06-10 23:38:11 +0200

    added shadow jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+   id("com.github.johnrengelman.shadow") version "8.1.1"
    kotlin("jvm") version "1.6.21"
 }
 
@@ -18,6 +19,12 @@ repositories {
 dependencies {
    compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
    implementation("com.github.twitch4j:twitch4j:1.15.0")
+   implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.10")
+   implementation("com.github.twitch4j:twitch4j:1.12.0")
+   implementation("io.github.openfeign:feign-okhttp:12.3")
+   implementation("io.github.openfeign:feign-slf4j:12.3")
+   implementation("io.github.openfeign:feign-jackson:12.3")
+   implementation("io.github.openfeign:feign-hystrix:12.3")
 }
 
 java {
